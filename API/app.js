3ff668c15b4778f38ce716881f398187957fb867
unknown <magnus01lysfjord@gmail.com>
2018-11-21 17:54:14 +0100

    added new unit testing library with matplotlib and over-rode the original compilebox'

--- API/app.js
+++ API/app.js
@@ -6,16 +6,21 @@
 */
 
 
+// added 13:03 to work with the gist-function for finding files by exxt.
+var path = require('path')
+var fs = require('fs')
+//
 
 
 var express = require('express');
+
 var http = require('http');
 var arr = require('./compilers');
 var sandBox = require('./DockerSandbox');
 var bodyParser = require('body-parser');
 var app = express();
 var server = http.createServer(app);
-var port=3020;
+var port=3010;
 
 
 var ExpressBrute = require('express-brute');
@@ -28,7 +33,7 @@ var bruteforce = new ExpressBrute(store,{
 app.use(express.static(__dirname));
 app.use(bodyParser());
 
-app.all('*', function(req, res, next) 
+app.all('*', function(req, res, next)
 {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -43,46 +48,111 @@ function random(size) {
 }
 
 
-app.post('/compile',bruteforce.prevent,function(req, res) 
+// From GIST, see browser.
+function recFindByExt(base,ext,files,result)
+{
+    files = files || fs.readdirSync(base)
+    result = result || []
+
+    files.forEach(
+        function (file) {
+            var newbase = path.join(base,file)
+            if ( fs.statSync(newbase).isDirectory() )
+            {
+                result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
+            }
+            else
+            {
+                if ( file.substr(-1*(ext.length+1)) == '.' + ext )
+                {
+                    result.push(newbase)
+                }
+            }
+        }
+    )
+    return result
+}
+
+
+app.post('/compile',bruteforce.prevent,function(req, res)
 {
 
     var language = req.body.language;
+
+
     var code = req.body.code;
+    var unit_test = req.body.unit_test;
+
+
+    //ADD ONTO UNIT TEST TO CODE VARABILE
+
+
     var stdin = req.body.stdin;
-   
+
     var folder= 'temp/' + random(10); //folder in which the temporary folder will be saved
     var path=__dirname+"/"; //current working path
     var vm_name='virtual_machine'; //name of virtual machine that we want to execute
-    var timeout_value=20;//Timeout Value, In Seconds
+    var timeout_value=10;//Timeout Value, In Seconds
 
     //details of this are present in DockerSandbox.js
-    var sandboxType =
-        new sandBox(
-            timeout_value,
-            path,
-            folder,
-            vm_name,
-            arr.compilerArray[language][0],
-            arr.compilerArray[language][1],
-            code,
-            arr.compilerArray[language][2],
-            arr.compilerArray[language][3],
-            arr.compilerArray[language][4],
-            stdin);
+    var sandboxType = new sandBox(
+        timeout_value,
+        path,
+        folder,
+        vm_name,
+        //  COMPILER NAME
+        arr.compilerArray[language][0],
+
+//  file_name,
+//         python
+        arr.compilerArray[language][1],
+
+        // code,
+        code,
+
+        // unit_test
+        unit_test,
+
+        // output_command,
+        // BLANK
+        arr.compilerArray[language][2],
+
+        // languageName,
+        // Python
+        arr.compilerArray[language][3],
+
+        // e_arguments,
+        //blank
+        arr.compilerArray[language][4],
+
+        // stdin_data,
+        stdin,
+
+
+        // unit_file_name
+        // unit_test.py
+        arr.compilerArray[language][5]
+    );
+
 
 
     //data will contain the output of the compiled/interpreted code
     //the result maybe normal program output, list of error messages or a Timeout error
     sandboxType.run(function(data,exec_time,err)
     {
-        //console.log("Data: received: "+ data)
-    	res.send({folder: "folderlocakation", output:data, langid: language,code:code, errors:err, time:exec_time});
+        console.log("Data: received: "+ data,exec_time,err)
+        // pick out images here, using regex ?, That way we can return them straight if they are found,
+        // possibly as a list of images, if there are multiple.
+
+        //var tmp_loc = something_from_script  (path + folder)
+        //var images = recFindByExt(tmp_loc, 'jpg') //should be possible to res.send this list now.
+        res.send({folder: "hello", output:data, langid: language,code:code, unit_test:unit_test, errors:err, time:exec_time});
     });
-   
+
 });
 
 
-app.get('/', function(req, res) 
+app.get('/', function(req, res)
 {
     res.sendfile("./index.html");
 });
--- Setup/Install_14.04.sh
+++ Setup/Install_14.04.sh
@@ -23,6 +23,7 @@ echo "NodeJS setup Complete"
 ###########################
 chmod 777 ../API/DockerTimeout.sh
 chmod 777 ../API/Payload/script.sh
+chmod 777 ../API/Payload/scriptunit.sh
 chmod 777 ../API/Payload/javaRunner.sh
 chmod 777 UpdateDocker.sh
 
