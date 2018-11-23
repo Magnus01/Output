f6c91477d6f16c648a6c575670a4fb78252b996c
unknown <magnus01lysfjord@gmail.com>
2018-11-23 12:43:40 +0100

    no cache

--- Setup/UpdateDocker.sh
+++ Setup/UpdateDocker.sh
@@ -1,5 +1,5 @@
 
 echo "Creating Docker Image"
-docker build -t 'virtual_machine' - < Dockerfile
+docker build  --no-cache -t 'virtual_machine' - < Dockerfile
 echo "Retrieving Installed Docker Images"
 docker images
