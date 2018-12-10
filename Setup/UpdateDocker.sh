f4008712e94847ddbf4192248728fe6fd63dbc0b
Magnus01 <magnus01lysfjord@gmail.com>
2018-12-10 17:18:04 +0100

    '

--- Setup/UpdateDocker.sh
+++ Setup/UpdateDocker.sh
@@ -1,6 +1,7 @@
 
 echo "Creating Docker Image"
-docker build . --no-cache -t 'virtual_machine' -f - < Dockerfile
+#docker build . --no-cache -t 'virtual_machine' -f - < Dockerfile
+docker build -t 'virtual_machine' - < Dockerfile
 #docker build --no-cache -t 'virtual_machine' - < Dockerfile
 echo "Retrieving Installed Docker Images"
 docker images
